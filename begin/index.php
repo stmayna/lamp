dfec90218d2ebc6be2b3a8229b60e06ad423efaf
stmayna <stmayna@gmail.com>
2022-10-12 15:44:07 +0800

    directory for test document

---
+++ begin/index.php
@@ -0,0 +1,46 @@
+<?php
+		$pdo = new PDO('mysql:host=localhost;dbname=ship_ports', 'root', 'password');
+
+        $sql = 'SELECT * FROM invoices Orders LIMIT 100';
+
+        $q = $pdo->query($sql);
+
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <table border="1">
+	        <thead>
+		    <tr>
+			<th>Invoice Number</th>
+			<th>Stock Code</th>
+			<th>Description</th>
+			<th>Quantity</th>
+            <th>InvoiceDate</th>
+			<th>Unit Price</th>
+            <th>Customer ID</th>
+            <th>Country</th>
+		    </tr>
+	        </thead>
+	    <tbody>
+        <?php while ($row = $q->fetch()): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['InvoiceNo']) ?></td>
+                            <td><?php echo htmlspecialchars($row['StockCode']); ?></td>
+                            <td><?php echo htmlspecialchars($row['Description']); ?></td>
+                            <td><?php echo htmlspecialchars($row['Quantity']) ?></td>
+                            <td><?php echo htmlspecialchars($row['InvoiceDate']); ?></td>
+                            <td><?php echo htmlspecialchars($row['UnitPrice']); ?></td>
+                            <td><?php echo htmlspecialchars($row['CustomerID']); ?></td>
+                            <td><?php echo htmlspecialchars($row['Country']); ?></td>
+                        </tr>
+        <?php endwhile; ?>
+	    </tbody>
+        </table>        
+    </body>
+</html>
