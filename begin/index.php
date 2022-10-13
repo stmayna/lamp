fc6f15a0ba8930353a6a3d6504e211dbd53013a4
stmayna <stmayna@gmail.com>
2022-10-13 10:06:40 +0800

    change thesql query with order by invoice date

--- begin/index.php
+++ begin/index.php
@@ -1,5 +1,5 @@
 <?php
-		$pdo = new PDO('mysql:host=localhost;dbname=ship_ports', 'root', 'password');
+        $pdo = new PDO('mysql:host=localhost;dbname=ship_ports', 'root', 'password');
 
         /*$sql = 'SELECT * FROM invoices Orders LIMIT 100';*/
         $sql = 'SELECT * FROM invoices ORDER BY InvoiceDate DESC LIMIT 30';
@@ -18,14 +18,14 @@
         <table border="1">
 	        <thead>
 		    <tr>
-			<th>Invoice Number</th>
-			<th>Stock Code</th>
-			<th>Description</th>
-			<th>Quantity</th>
-            <th>InvoiceDate</th>
-			<th>Unit Price</th>
-            <th>Customer ID</th>
-            <th>Country</th>
+                <th>Invoice Number</th>
+                <th>Stock Code</th>
+                <th>Description</th>
+                <th>Quantity</th>
+                <th>InvoiceDate</th>
+                <th>Unit Price</th>
+                <th>Customer ID</th>
+                <th>Country</th>
 		    </tr>
 	        </thead>
 	    <tbody>
