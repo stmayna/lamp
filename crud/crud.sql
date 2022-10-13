d19bf43e71d8584b452591b1afc133ed3266ad05
stmayna <stmayna@gmail.com>
2022-10-13 09:12:06 +0800

    more detail about inserting the data

--- crud/crud.sql
+++ crud/crud.sql
@@ -5,11 +5,11 @@ VALUES ('777778','88889','This is the description 2',5,'2014-02-12 08:35:00', 4.
 
 -- Result: This data inserted into the last row
 -- based on the material that I read
--- there is now way in inserting the data into the first row specifically
--- but we can cheat while playing with viewing the data, using select
+-- there is no way in inserting the data into the first row specifically
+-- but we can cheat while viewing the data, using select
 -- well, this data has no key in the very beginning, but it has date column
 -- so the newest one must be the last inserted one, the most current date
--- we can ORDER BY using InvoiceDate column  
+-- we can ORDER BY it using InvoiceDate column  
 
 -- Checking the inserted data from the last row.
 SELECT * FROM invoices ORDER BY InvoiceDate DESC LIMIT 1;
